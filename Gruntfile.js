75f572cb2dd2d79bd32d2ea04fd6866a62622136
p.scheit <p.scheit@ps-webforge.com>
2012-11-28 10:41:33 +0100

    add the ability to create a new class

--- Gruntfile.js
+++ Gruntfile.js
@@ -131,6 +131,53 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'connect', 'qunit', 'requirejs']);
   grunt.registerTask('test', ['connect', 'qunit']);
   grunt.registerTask('travis', ['jshint', 'connect', 'qunit']);
+  
+  grunt.registerTask("create-class", "crates a new Class Stub", function (className, isa, traits) {
+    var _ = grunt.util._;
+    grunt.log.writeln('');
+    
+    isa = isa === '' ? undefined : isa;
+    var classParts = className.split('.');
+    var ns = classParts.slice(0, -1);
+    var classOnlyName = classParts.slice(-1);
+    var nsDir = 'lib/'+ns.join('/');
+    var file = nsDir+'/'+classOnlyName+'.js';
+    
+    if (!grunt.file.exists(file) || grunt.cli.options.overwrite) {
+      traits = _.rest(arguments, 2);
+  
+      var deps = traits;
+      
+      if (isa) {
+        deps.push(isa);
+      }
+      
+      var jsStub = _.template(
+        grunt.file.read('lib/ClassTemplate.js'), {
+          _: _,
+          traits: traits.join(', '),
+          className: className,
+          isa: isa,
+          deps: _.unique(deps)
+        }
+      );
+    
+      
+      if (!grunt.file.isDir(nsDir)) {
+        grunt.file.mkdir(nsDir);
+      }
+    
+      grunt.file.write(file, jsStub);
+      
+      grunt.log.writeln('written new class to file '+file);
+      grunt.log.ok();
+      
+      return;
+    } else {
+      grunt.log.error('will not write to existing file: '+file);
+      return false;
+    }
+  });
 
   grunt.registerMultiTask("update-tests", "updates the index / and singleTestFiles for all test files", function() {
     var filepaths = grunt.file.expandFiles(grunt.util._.pluck(this.files, 'src'));
