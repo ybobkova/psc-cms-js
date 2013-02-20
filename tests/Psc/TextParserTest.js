b8c561c7ae863b70d5a4afe4f59b7d9d59c9b7f6
Philipp Scheit <p.scheit@ps-webforge.com>
2013-02-20 14:43:49 +0100

    add failing tests for textparser

--- tests/Psc/TextParserTest.js
+++ tests/Psc/TextParserTest.js
@@ -6,13 +6,26 @@ define(['psc-tests-assert','Psc/TextParser'], function(t) {
     var textParser = new Psc.TextParser({ });
     
     var lines = [
-      "sichergestellt, dass Nachhaltigkeit versprechende Lösungen aus der Region Leuchtturmwirkung erzielen und z",
+      "sichergestellt, dass Nachhaltigkeit versprechende Lösungen aus der Region Leuchtturmwirkung erzielen und",
       "maghrebinischen Städten ergänzt, so dass das Wissen und die Erfahrungen deutscher Kommunen,",
       "CoMun bietet für die Partner eine Kombination aus Dialog, Erfahrungsaustausch und technischer Beratung. Das Programm  arbeitet",
-      "owie verschiedenen deutschen Städten, dem Marseille Center for Mediterranean Integration (CMI"
+      "sowie verschiedenen deutschen Städten, dem Marseille Center for Mediterranean Integration (CMI"
     ];
     
-    test = t.setup(test, {textParser: textParser, lines: lines});
+    var listPoints = [
+      "Familienunternehmen mit mehr als 30 Jahren Erfahrung und Wissen",
+      "Unternehmergeist durch und durch – langfristige Fortführung des Unternehmens durch Söhne des Firmengründers",
+      "Mehr als 12 000 zufriedene Kunden weltweit",
+      "Umfangreiche Innovationskraft und Erfindertum"
+    ];
+    
+    var numberedListText =
+      "1\tFamilienunternehmen mit mehr als 30 Jahren Erfahrung und Wissen \n"+
+      "2\tUnternehmergeist durch und durch – langfristige Fortführung des Unternehmens durch Söhne des Firmengründers    \n"+
+      "3\tMehr als 12 000 zufriedene Kunden weltweit\n"+
+      "4\tUmfangreiche Innovationskraft und Erfindertum      \n";
+  
+    test = t.setup(test, {textParser: textParser, lines: lines, listPoints: listPoints, numberedListText: numberedListText});
     
     test.assertParsing = function(expectedParsedStructure, text) {
       test.assertEquals(
@@ -38,7 +51,7 @@ define(['psc-tests-assert','Psc/TextParser'], function(t) {
     );
   });
 
-  test("parser detects a list surrounded from text", function() {
+  test("parser detects a list surounded by text", function() {
     setup(this);
     
     this.assertParsing([
@@ -52,8 +65,8 @@ define(['psc-tests-assert','Psc/TextParser'], function(t) {
       this.lines[0]+"\n\n"+
       this.lines[1]+"\n\n"+
       "Mit dem Vorhaben CoMun fördert die:\n"+
-      "-	list1\n"+
-      "-	list2\n"+
+      "-\tlist1\n"+
+      "-\tlist2\n"+
       "\n"+
       this.lines[2]
     );
@@ -62,16 +75,68 @@ define(['psc-tests-assert','Psc/TextParser'], function(t) {
   test("parser detects list only in lines", function() {
     setup(this);
     
-    this.assertEquals([
-      {value: ["list1", "list2", "list3"], type:"list"}
+    this.assertParsing([
+        {value: ["list1", "list2", "list3"], type:"list"}
       ],
       
-      this.textParser.parse(
-        "\n"+
-        " - list1\n"+
-        " - list2\n"+
-        " - list3\n"
-      )
+      "\n"+
+      " - list1\n"+
+      " - list2\n"+
+      " - list3\n"
     );
+  });
+
+  test("parser detects a wrong formatted headline between paragraph and a list", function() {
+    setup(this);
+    
+    this.assertParsing([
+        {value: "Nach Klick auf „Partner“ erscheinen zwei Unterrubriken:", type: 'paragraph'},
+        {value: ["Technologie", "Internationale Vertriebs-Partner"], type: 'list'},
+        
+        {value: "Nach Klick auf „Technologie-Partner“ erscheint folgender Text", type: 'paragraph'},
+        
+        {value: "Kanefusa\n"+
+                "Kanefusa ist Japans größter Hersteller von Qualitäts-Werkzeugen für die Branchen Metall,\n"+
+                "als überaus verlässlich und langlebig erwiesen.\n",
+         type: 'paragraph'
+        }
+      ],
+      
+      "Nach Klick auf „Partner“ erscheinen zwei Unterrubriken: \n"+
+      "•\tTechnologie \n"+
+      "•\tInternationale Vertriebs-Partner   \n"+
+      "\n"+
+      "Nach Klick auf „Technologie-Partner“ erscheint folgender Text \n"+
+      "\t\n"+
+      "\tKanefusa \n"+
+      "\tKanefusa ist Japans größter Hersteller von Qualitäts-Werkzeugen für die Branchen Metall, \n"+
+      "\tals überaus verlässlich und langlebig erwiesen. \n"+
+      "\n"
+    );
+  });
+  
+  test("parser detects a correctly formatted headline before an indented block", function() {
+    setup(this);
+    
+    this.assertParsing([
+        {value: "Nach Klick auf „Partner“ erscheinen zwei Unterrubriken:", type: 'paragraph'},
+        {value: ["Technologie", "Internationale Vertriebs-Partner"], type: 'list'},
+        
+        {value: "Nach Klick auf „Technologie-Partner“ erscheint folgender Text", type: 'paragraph'},
+        
+        {value: "Kanefusa", type: 'headline'},
+        {value: 
+                "Kanefusa ist Japans größter Hersteller von Qualitäts-Werkzeugen für die Branchen Metall,"+
+                "Holzbearbeitung, Papier und Kunststoff. Alle hergestellten Sägeblätter, Schneiden und",
+         type: 'paragraph'
+        }
+      ],
+
+      "Kanefusa \n"+
+      "\tKanefusa ist Japans größter Hersteller von Qualitäts-Werkzeugen für die Branchen Metall, \n"+
+      "\tHolzbearbeitung, Papier und Kunststoff. Alle hergestellten Sägeblätter, Schneiden und \n"
+    );
+    
+    // die headline ist dadurch erkennbar, dass die nächsten Zeilen eingrückt sind mit einem tab
   });
 });
