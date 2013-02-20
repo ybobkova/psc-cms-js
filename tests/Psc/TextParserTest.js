7a2175ec1a37ff90e8114d8291241ff1d2bb55d1
Philipp Scheit <p.scheit@ps-webforge.com>
2013-02-20 12:29:48 +0100

    make the TextParser text a little bit prettier

--- tests/Psc/TextParserTest.js
+++ tests/Psc/TextParserTest.js
@@ -5,53 +5,61 @@ define(['psc-tests-assert','Psc/TextParser'], function(t) {
   var setup = function(test) {
     var textParser = new Psc.TextParser({ });
     
-    var texts = [
+    var lines = [
       "sichergestellt, dass Nachhaltigkeit versprechende Lösungen aus der Region Leuchtturmwirkung erzielen und z",
       "maghrebinischen Städten ergänzt, so dass das Wissen und die Erfahrungen deutscher Kommunen,",
       "CoMun bietet für die Partner eine Kombination aus Dialog, Erfahrungsaustausch und technischer Beratung. Das Programm  arbeitet",
       "owie verschiedenen deutschen Städten, dem Marseille Center for Mediterranean Integration (CMI"
     ];
     
-    return t.setup(test, {textParser: textParser, texts: texts});
+    test = t.setup(test, {textParser: textParser, lines: lines});
+    
+    test.assertParsing = function(expectedParsedStructure, text) {
+      test.assertEquals(
+        expectedParsedStructure,
+        textParser.parse(text)
+      );
+    };
   };
 
   test("parser extracts test paragraph into nodes", function() {
     setup(this);
     
-    this.assertEquals([
-        {value: this.texts[0], type: "paragraph"},
-        {value: this.texts[1], type: "paragraph"},
-        {value: this.texts[2], type: "paragraph"}
+    this.assertParsing(
+      [
+        {value: this.lines[0], type: "paragraph"},
+        {value: this.lines[1], type: "paragraph"},
+        {value: this.lines[2], type: "paragraph"}
       ],
-      this.textParser.parse(this.texts[0]+"\n\n"+
-                            this.texts[1]+"\n\n"+
-                            this.texts[2]+"\n"
-                           )
+      
+      this.lines[0]+"\n\n"+
+      this.lines[1]+"\n\n"+
+      this.lines[2]+"\n"
     );
   });
 
-  test("parser detects lists in texts", function() {
+  test("parser detects a list surrounded from text", function() {
     setup(this);
     
-    this.assertEquals([
-        {value: this.texts[0], type: "paragraph"},
-        {value: this.texts[1], type: "paragraph"},
+    this.assertParsing([
+        {value: this.lines[0], type: "paragraph"},
+        {value: this.lines[1], type: "paragraph"},
         {value: "Mit dem Vorhaben CoMun fördert die:", type: "paragraph"},
         {value: ["list1", "list2"], type: "list"},
-        {value: this.texts[2], type: "paragraph"}
+        {value: this.lines[2], type: "paragraph"}
       ],
-      this.textParser.parse(this.texts[0]+"\n\n"+
-                            this.texts[1]+"\n\n"+
-                            "Mit dem Vorhaben CoMun fördert die:\n"+
-                            "-	list1\n"+
-                            "-	list2\n"+
-                            "\n"+
-                            this.texts[2]
-                           )
+
+      this.lines[0]+"\n\n"+
+      this.lines[1]+"\n\n"+
+      "Mit dem Vorhaben CoMun fördert die:\n"+
+      "-	list1\n"+
+      "-	list2\n"+
+      "\n"+
+      this.lines[2]
     );
   });
   
-  test("parser detects list only in texts", function() {
+  test("parser detects list only in lines", function() {
     setup(this);
     
     this.assertEquals([
