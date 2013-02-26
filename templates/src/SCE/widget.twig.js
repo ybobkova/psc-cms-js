ab28800ebf543d8aeb894193487e668068eb06a1
Philipp Scheit <p.scheit@ps-webforge.com>
2013-02-26 08:18:07 +0100

    add composer vendor dir to composer.json

--- templates/src/SCE/widget.twig.js
+++
@@ -1,6 +0,0 @@
-define(["twig"], function (Twig) {
-	var twig = Twig.twig;
-twig({id:"templates/src/SCE/widget.twig", data:[{"type":"raw","value":"<div class=\"widget ui-widget ui-widget-content ui-helper-clearfix ui-corner-all\" style=\"margin-bottom: 5px;\">\n  <h3 class=\"widget-header ui-helper-reset ui-state-default ui-corner-all\" style=\"padding: 0.5em 0.5em 0.5em 0.7em;\">\n    <span class=\"ui-icon ui-icon-close\" data-bind=\"click: close\" style=\"float: right; cursor: pointer;\"></span>Absatz\n  </h3>\n  \n  <div class=\"widget-content\" style=\"padding: 1.1em;\">\n  </div>\n</div>"}], precompiled: true});
-
-	return templates;
-});
