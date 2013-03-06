007edc2f905681810926aac290c9e57f02c061bc
Philipp Scheit <p.scheit@ps-webforge.com>
2013-03-06 12:32:42 +0100

    fix texteditor getCaret und setSelection

---
+++ vendor/stackoverflow/jquery-selectrange.js
@@ -0,0 +1,17 @@
+define(['jquery'], function ($) {
+  // http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area#841121
+  return $.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+  };
+});
