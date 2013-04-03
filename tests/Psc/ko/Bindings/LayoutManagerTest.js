1b7fdc114bc30f5416fd4be84a3ef937e68098e0
Philipp Scheit <p.scheit@ps-webforge.com>
2013-04-03 14:01:24 +0200

    define the bindings in the layoutManager bindings and test these

--- tests/Psc/ko/Bindings/LayoutManagerTest.js
+++ tests/Psc/ko/Bindings/LayoutManagerTest.js
@@ -1,25 +1,88 @@
-define(['psc-tests-assert', 'knockout', 'Psc/Test/DoublesManager', 'Psc/ko/Bindings/LayoutManager'], function(t, ko) {
+define(['psc-tests-assert', 'knockout', 'test-files/navigation.comun.flat', 'Psc/UI/NavigationSelect', 'Psc/Test/DoublesManager', 'Psc/ko/Bindings/LayoutManager'], function(t, ko, comunFlat) {
   
   module("Psc.ko.Bindings.LayoutManager");
 
   var setup = function (test) {
     var dm = new Psc.Test.DoublesManager();
+
+    dm.getContainer().getNavigationService().setFlat(comunFlat); // better acceptance
+
     var bindings = new Psc.ko.Bindings.LayoutManager({
       container: dm.getContainer(),
       knockout: ko,
       uploadService: dm.getUploadService()
     });
-    
-    return t.setup(test, {bindings: bindings, bindingNames: ['singleImage', 'navigationSelect']});
+
+    bindings.activate();
+
+    return t.setup(test, {
+      bindings: bindings, 
+      bindingNames: ['singleImage', 'navigationSelect']
+    });
   };
   
+
   test("injects some bindings into layoutManager", function() {
     var that = setup(this);
-
-    that.bindings.activate();
 
     for (var i = 0; i<this.bindingNames.length; i++) {
       this.assertNotUndefined(ko.bindingHandlers[ this.bindingNames[i] ], 'binding "'+this.bindingNames[i]+'" is injected');
     }
+  });
+
+  test("navigationSelect init creates navigationSelect for element", function () {
+    var $element = $('<div />'), that = t.setupBinding(setup(this), 'navigationSelect', $element);
+
+    this.koInit();
+    stop();
+
+    // is ko able to notice async initialisiations?
+    // should we trigger some finish here, etc?
+    // can we bind to k.o. event?
+    setTimeout(function () {
+      start();
+      that.assertHasJooseWidget(Psc.UI.NavigationSelect, $element);
+
+    }, 50);
+  });
+
+  test("navigationSelect init sets current nav node with just integer value", function () {
+    var $element = $('<div />'), that = t.setupBinding(setup(this), 'navigationSelect', $element);
+
+    this.koInit(15);
+    stop();
+
+    // is ko able to notice async initialisiations?
+    // should we trigger some finish here, etc?
+    // can we bind to k.o. event?
+    setTimeout(function () {
+      start();
+      var navigationSelect = that.assertHasJooseWidget(Psc.UI.NavigationSelect, $element);
+
+      that.assertEquals({
+        label: " / Wer ? / Unser Lernnetzwerk",
+        value: 15
+      }, navigationSelect.getValue());
+
+    }, 50);
+  });
+
+  test("is ko working when value is changed with val() (and then trigger())", function () {
+    var that = setup(this);
+    var $ta;
+
+    $('#visible-fixture').empty().append($ta = $('<textarea data-bind="value: t1.value" />'));
+
+    var model = {
+      t1: {
+        value: ko.observable("da value")
+      }
+    };
+
+    ko.applyBindings(model ,$('#visible-fixture')[0]);
+
+    $ta.val('new val').trigger('change'); // trigger is necessary to make it work
+
+    this.assertEquals("new val", ko.utils.unwrapObservable(model.t1.value));
   });
 });
