ea2551a046665ce4cba9da6053cef794e90d2064
Philipp Scheit <p.scheit@ps-webforge.com>
2013-04-03 14:01:37 +0200

    allow the navigationselect to read and write its value

--- tests/Psc/UI/NavigationSelectTest.js
+++ tests/Psc/UI/NavigationSelectTest.js
@@ -7,7 +7,8 @@ define(['psc-tests-assert', 'test-files/navigation.comun.flat', 'Psc/UI/Navigati
     var navigationSelect = new Psc.UI.NavigationSelect({
       flat: comunFlat,
       widget: $('#visible-fixture').empty(),
-      displayLocale: 'de'
+      displayLocale: 'de',
+      selected: { label: "can be rubbish", value: 15}
     });
     
     return t.setup(test, {navigationSelect: navigationSelect});
@@ -20,6 +21,30 @@ define(['psc-tests-assert', 'test-files/navigation.comun.flat', 'Psc/UI/Navigati
 
     var $input = this.assertjQueryLength(1, $widget.find('input'));
     //this.assertjQueryHasWidget('', $input, message);
+  });
 
+  test("selected passed will select the current item / value, etc", function () {
+    var that = setup(this);
+    var $input = this.assertjQueryLength(1, this.navigationSelect.unwrap().find('input'));
+
+    this.assertEquals('can be rubbish', $input.val());
+
+    this.assertEquals({label: 'can be rubbish', value: 15}, this.navigationSelect.getValue());
+  });
+
+  test("selectedNodeId passed will select the current item just with value", function () {
+    var that = setup(this);
+
+    var navigationSelect = new Psc.UI.NavigationSelect({
+      flat: comunFlat,
+      widget: $('#visible-fixture').empty(),
+      displayLocale: 'de',
+      selectedNodeId: 15
+    });
+
+    that.assertEquals({
+       label: " / Wer ? / Unser Lernnetzwerk",
+       value: 15
+    }, navigationSelect.getValue());
   });
 });
