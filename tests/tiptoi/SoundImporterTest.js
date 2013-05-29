e1200b4b619d8df957e88c142fa49ca6e9e7ed49
Philipp Scheit <p.scheit@ps-webforge.com>
2013-05-29 11:00:15 +0200

    soundimporter WIP

--- tests/tiptoi/SoundImporterTest.js
+++ tests/tiptoi/SoundImporterTest.js
@@ -1,18 +1,70 @@
-define(['psc-tests-assert','tiptoi/SoundImporter','Psc/Test/DoublesManager'], function(t) {
+define(['psc-tests-assert', 'jquery', 'tiptoi/SoundImporter', 'tiptoi/Sound','Psc/Test/DoublesManager'], function(t, $) {
   
   module("tiptoi.SoundImporter");
   
   var setup = function (test) {
-    //var dm = new Psc.Test.DoublesManager();
-    var soundImporter;// = new tiptoi.SoundImporter({ });
+    var dm = new Psc.Test.DoublesManager();
+
+    var hs = "Pascal Breuer";
+
+    var $widget = $('#visible-fixture').empty();
+
+    var Backend = function () {
+      var that = this;
+
+      $.extend(this, {
+
+        createSound: function (content, number, speakers, status, details) {
+          return {
+            status: status,
+            details: details || [],
+            sound: new tiptoi.Sound({
+              content: content,
+              number: number,
+              speakers: speakers
+            })
+          };
+        },
+
+        fakeGetParsedSounds: function () {
+          return [
+            that.createSound("Dieser Sound ist neu ohne Soundnummer", null, [hs], "new"),
+            that.createSound("Dieser Sound ist neu mit Soundnummer", "2-HRF-002", [hs], "new"),
+
+            that.createSound("Dieser Sound ist vorhanden mit Soundnummer", "2-TEST-201", ["Paul"], "existing"),
+            that.createSound("Dieser Sound ist vorhanden mit falscher Soundnummer", "2-TEST-2001", ["Paul"], ["wrong_number"], ["Der Inhalt wurde in der Datenbank unter einer anderen Nummer gefunden. Gefundene Nummer: 2-TEST-201]"]),
+
+            that.createSound("Dieser Sound ist vorhanden mit falschen Sprechern", "2-TEST-201", [hs], ["wrong_speakers"], ["Der Sound mit der Nummer 2-TEST-201 hat als Sprecher 'Pascal Breuer' eingetragen, der Sprecher in der Datenbank ist jedoch 'Paul'"])
+          ];
+        }
+      });
+    };
+
+    var backend = new Backend();
+
+    var soundImporter = new tiptoi.SoundImporter({ 
+      backend: backend,
+      widget: $widget,
+      container: dm.getContainer(),
+      inputSounds: backend.fakeGetParsedSounds()
+    });
     
-    t.setup(test, {soundImporter: soundImporter});
+    return t.setup(test, {importer: soundImporter, $table: $widget.find('> .psc-cms-ui-group > div.content > table:first')});
   };
 
-  test("acceptance", function() {
-    setup(this);
-    expect(0);
-  
-    // this.soundImporter.doSomething();
+  test("widget is rendered", function() {
+    var that = setup(this);
+    var $widget = this.importer.unwrap();
+
+    var $group = this.assertjQueryLength(1, $widget.find('> .psc-cms-ui-group'));
+    var $table = this.assertjQueryLength(1, $group.find('> div.content > table:first'));
+
+    this.assertSame(that.$table.get(0), $table.get(0));
+  });
+
+  test("table contains all rows from inputSounds", function () {
+    var that = setup(this);
+
+    this.assertjQueryLength(5, that.$table.find('> tbody > tr'));
   });
 });
