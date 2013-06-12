0ce0b4ed51a54e00797e291b4060f33e21a0f961
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-12 11:53:14 +0200

    translate pages menu

--- tests/Psc/UI/PagesMenuTest.js
+++ tests/Psc/UI/PagesMenuTest.js
@@ -19,6 +19,7 @@ define(['psc-tests-assert', 'fixtures/navigation.comun.flat', 'jquery-simulate',
       },
 
       openTabsSelection: function (title, tabButtonItems, dialogAttributes) {
+        console.log(title);
         test.selection = {
           title: title,
           tabButtonItems: tabButtonItems,
@@ -45,7 +46,8 @@ define(['psc-tests-assert', 'fixtures/navigation.comun.flat', 'jquery-simulate',
     var pages = new Psc.UI.PagesMenu({
       uiController: uiController,
       widget: $widget,
-      flat: flat
+      flat: flat,
+      locale: 'de'
     });
     
     return t.setup(test, {pages: pages, $widget: $widget, openedTabs: [], selection: undefined});
@@ -73,7 +75,7 @@ define(['psc-tests-assert', 'fixtures/navigation.comun.flat', 'jquery-simulate',
         that.assertEquals(3, that.selection.tabButtonItems.length, 'cs1, cs2 and page buttons are avaible');
         
         start();
-      }, 20); 
+      }, 100); 
       
       start();
     });
