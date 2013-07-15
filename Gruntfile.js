c622fcabdc94a926bb67f0d6e2eef518c25b6b65
Philipp Scheit <p.scheit@ps-webforge.com>
2013-07-15 09:24:20 +0200

    webpack first try with no success

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
 //  grunt.loadNpmTasks('grunt-contrib-uglify');
 //  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-hogan');
+  grunt.loadNpmTasks('grunt-webpack');
 
   var port = 8000;
   var hostname = 'localhost';
@@ -182,6 +183,76 @@ module.exports = function(grunt) {
           */
           optimize: "none"
         }
+      }
+    },
+
+    webpack: {
+      options: {},
+      someName: {
+        entry: "./lib/boot-helper.js",
+        output: {
+          path: "build/",
+          filename: "[hash].js",
+          libraryTarget: "umd"
+        },
+
+        resolve: {
+          root: './lib/',
+          alias: {
+    'jquery': "../vendor/jquery/jquery-1.8.2",
+    'jquery-ui': "../vendor/jquery-ui/jquery-ui-1.8.24.custom.patched",
+    'jquery-ui-i18n': "../vendor/jquery-ui/jquery-ui-i18n.custom",
+    "qunit": "../vendor/qunit/qunit-1.10.0",
+    'joose': "../vendor/joose/all",
+    'ace': "../vendor/ace/lib/ace",
+    'lodash': "../vendor/lodash/lodash-0.10.0.min",
+    'psc-tests-assert': '../vendor/qunit-assert/lib/assert',
+    'qunit-assert': '../vendor/qunit-assert/lib/assert',
+    'TestRunner': "../vendor/qunit-assert/lib/TestRunner",
+    'img-files': '../img',
+    'jquery-form': "../vendor/jquery-form/jquery.form-3.20",
+    'jquery-fileupload': "../vendor/jquery-fileupload/jquery.fileupload",
+    'jquery-iframe-transport': "../vendor/jquery-fileupload/jquery.iframe-transport",
+    'jquery.ui.widget': "../vendor/jquery-fileupload/vendor/jquery.ui.widget",
+    'jquery-tmpl': "../vendor/jquery-tmpl/jquery.tmpl",
+    'jqwidgets': "../vendor/jqwidgets/jqx-all.min",
+    'jquery-simulate': "../vendor/jquery-simulate/jquery.simulate.patched",
+    'jquery-rangyinputs': "../vendor/jquery-rangyinputs/rangyinputs_jquery.min",
+    'jquerypp': "../vendor/jquerypp/1.0.0/amd/jquerypp",
+    'ui-connect-morphable': "../vendor/webforge/jquery.ui.connect-morphable",
+    'ui-paging': "../vendor/webforge/ui.paging",
+    "JSON": "../vendor/json/json2",
+    "hogan": "../vendor/hogan/hogan-2.0.0.min.amd",
+    'placeholder': "../vendor/mths.be/placeholder-2.0.6",
+    'stacktrace': "../vendor/eriwen/stacktrace-min-0.4",
+    'twitter-bootstrap': "../vendor/twitter-bootstrap/bootstrap",
+    'twitter-typeahead': "../vendor/twitter/typeahead/typeahead.min",
+    'knockout': "../vendor/knockout/knockout-2.2.1",
+    'knockout-mapping': "../vendor/knockout/knockout.mapping",
+    'test-files': "../tests/files",
+    'knockout-bindings': "lib/Psc/ko/bindings",
+    'jquery-selectrange': "../vendor/stackoverflow/jquery-selectrange",
+    'jquery-global': "../vendor/jqwidgets/globalization/jquery.global",
+    'jquery-global-de-DE': "../vendor/jqwidgets/globalization/jquery.glob.de-DE",
+    'templates': "../templates",
+    'html5shiv': "../vendor/afarkas/html5shiv",
+    'i18next': '../vendor/i18next/i18next.amd.withJQuery-1.6.3.min'
+
+          }
+        },
+
+        stats: {
+      // Configure the console output
+          colors: true,
+          modules: true,
+          reasons: true
+        },
+
+        //storeStatsTo: "xyz", // writes the status to a variable named xyz
+        // you may use it later in grunt i.e. <%= xyz.hash %>
+
+        failOnError: true // don't report error to grunt if webpack find errors
+        // Use this if webpack errors are tolerable and grunt should continue
       }
     }
   });
