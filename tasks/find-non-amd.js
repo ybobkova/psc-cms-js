a46e8d5ca104a1410c36e25f5177034841a4fcaa
Philipp Scheit <p.scheit@ps-webforge.com>
2013-07-18 10:00:12 +0200

    refactor gruntfile into tasks

---
+++ tasks/find-non-amd.js
@@ -0,0 +1,28 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask("find-non-AMD", "finds all files without a define() header for AMD", function () {
+    var filepaths = grunt.file.expand(grunt.util._.pluck(this.files, 'src'));
+    var _ = grunt.util._;
+
+    grunt.log.writeln('Searching in '+filepaths.length+' files.');
+    var error = false;
+
+    _.each(filepaths, function(path) {
+      var code = grunt.file.read(path);
+
+      if (!code.match(/^\s*define\(/m) && !code.match(/("|')function("|')\s*\=\=\=?\s*typeof define&&define\.amd/)) {
+        grunt.log.error('File '+path+' contains not an AMD define');
+        error = true;
+      }
+
+    });
+
+    if (error) {
+      grunt.log.fail();
+    } else {
+      grunt.log.ok();
+    }
+  });
+
+};
