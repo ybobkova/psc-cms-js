af76cd59d5aa89897ad7f252032643f502d1bd21
Philipp Scheit <p.scheit@ps-webforge.com>
2013-07-30 16:58:53 +0200

    refactor some init values tobe cojoko readable

--- tests/Psc/UI/ResizableImageTest.js
+++ tests/Psc/UI/ResizableImageTest.js
@@ -14,15 +14,11 @@ define(['psc-tests-assert','Psc/UI/ResizableImage'], function(t) {
     return t.setup(test, {resizableImage: resizableImage, $widget: $widget});
   };
 
-  test("source refreshing", function() {
-    expect(0);
-    setup(this);
-    
-    var img = this.resizableImage;
-  
-    setTimeout(function () {
-      img.refreshSource('http://cms.comun.laptop.ps-webforge.net/images/a/a07295eea7fc3c00a3b1115a626f316c7714e698.png');
-    }, 1100);
-    
+  test("after construct has handles init", function () {
+    var that = setup(this);
+
+    this.assertNotUndefined(that.resizableImage.getHandles());
+
   });
+
 });
