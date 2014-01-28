16d9b24640e871ac8b391f8e2669ab377573a7b9
Philipp Scheit <p.scheit@ps-webforge.com>
2014-01-28 16:36:49 +0100

    remove sync button from tito editor

--- tests/tiptoi/TitoGameEditorTest.js
+++ tests/tiptoi/TitoGameEditorTest.js
@@ -52,23 +52,6 @@ define(
                 body: titoHighlighted,
                 code: 200
               }));
-            } else if (request.getUrl() === '/api/product/test/tito/synchronize') {
-              if (options.synchronizeResponse) {
-                if (options.synchronizeResponse.getCode() === 200) {
-                  d.resolve(options.synchronizeResponse);
-                } else {
-                  d.reject(options.synchronizeResponse);
-                }
-              } else {
-                d.resolve(new Psc.AjaxResponse({
-                  request: request,
-                  body: {
-                    tito: syncedTito,
-                    log: "alles hat geklappt"
-                  },
-                  code: 200
-                }));
-              }
             } else {
               d.reject(new Psc.AjaxResponse({
                 request: request,
@@ -116,59 +99,5 @@ define(
       
       start();
     });
-  });
-  
-  asyncTest('when clicked on sync button, tito sync is loaded into pre', function () {
-    var that = setup(this);
-    
-    var initialLoad = true;
-    
-    // first load
-    that.evm.on('code-loaded', function () {
-      
-      if (initialLoad) {
-        stop();
-        initialLoad = false;
-        var $button = that.assertjQueryLength(1, that.$widget.find('button.psc-cms-ui-button.sync-button'), 'sync button is found');
-
-        start();
-        $button.simulate('click');
-      } else {
-        var $pre = that.assertjQueryLength(1, that.$widget.find('div.content pre'));
-        
-        // das geht nicht weil wir den service für get highlighted ja gemocked haben!
-        //that.assertContains('(2-FEE_023)', $pre.text(), 'the synced sound nummer is in text for pre');
-        
-        start();
-      }
-    });
-  });
-  
-  asyncTest("when synchronize response does not work a dialog is shown", function () {
-    var that = setup(this, {
-      synchronizeResponse: new Psc.Response({
-        body: syncExceptionText,
-        code: 500,
-        header: {
-          'X-Psc-CMS-Error-Message': "In der Datenbank wurde '2-EAE_0620' als Nummer gefunden, es wurde jedoch '2-DRA_0736' angegeben.. #8761 Text: 'Probiere es einfach noch mal' D:\\www\\tiptoi\\Umsetzung\\base\\src\\tiptoi\\SoundPersister.php:289",
-          'X-Psc-CMS-Error': 'true'
-        }
-      })
-    });
-
-    var $button = that.assertjQueryLength(1, that.$widget.find('button.psc-cms-ui-button.sync-button'), 'sync button is found');
-
-    var initialLoad = true;
-    
-    that.evm.on('error', function (e, response, dialog) {
-      that.assertTrue(dialog.isOpen(), 'dialog is open');
-      
-      dialog.close();
-      that.assertEquals(500, response.getCode());
-      
-      start();
-    });
-
-    $button.simulate('click');
   });
 });
